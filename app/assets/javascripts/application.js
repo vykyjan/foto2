4419375d3f4ff4bfc4b81137c28a783d542ea47d
Hanes <vykyja@seznam.cz>
2012-09-30 20:08:58 +0200

    java

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,24 +16,5 @@
 //= require bootstrap
 //= require_tree .
 
-//= require jquery.ui.widget
 
-//= require jquery.Jcrop
-
-//= require load-image.min
-//= require canvas-to-blob.min
-
-//= require jquery.iframe-transport
-//= require jquery.fileupload
-//= require jquery.fileupload-ip
-//= require jquery.fileupload-ui
-//= require locale
-//= require main
-
-//= require pictures
-
-//= require slimbox2
-
-//= require twitter/bootstrap
-//= require bootstrap-image-gallery.min
 
